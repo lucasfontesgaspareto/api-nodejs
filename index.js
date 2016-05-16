2229db0de850389975f336372497dcba00fea64d
Lucas Fontes Gaspareto <devgaspa@gmail.com>
2016-05-16 19:21:51 -0300

    Task #0002 - Construction of first feature
    - Creation of server on port 3000 in file index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-'use strict
+'use strict'
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+   res.send('Hello World!'); 
+});
+
+app.listen(PORT, () => {
+    console.log(`Connection on ${PORT}`);
+});
