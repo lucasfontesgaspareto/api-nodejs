077e3d4f09120f530ff65f187a15401f87e7000e
Lucas Fontes Gaspareto <devgaspa@gmail.com>
2016-05-16 19:32:21 -0300

    Task #0003 - Update on first feature
    - Implemetation of "/tasks" endpoint

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-   res.send('Hello World!'); 
+app.get('/', (req, res) => res.json({status: "nodeapi"}));
+
+app.get('/tasks', (req, res) => {
+   res.json({
+       tasks: [{
+           title: "Fazer compras"
+       }, {
+           title: "Consertar o pc"
+       }]
+   });
 });
 
-app.listen(PORT, () => {
-    console.log(`Connection on ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Connection on ${PORT}`));
